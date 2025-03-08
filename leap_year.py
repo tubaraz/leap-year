71aa9d27468ffcfc9d6aeaf72e047658d09093e2
tubaraz <tubaraz18@gmail.com>
2025-03-08 14:53:00 +0300

    Tuba Sümeyye Araz

--- leap_year.py
+++ leap_year.py
@@ -1,15 +1,19 @@
 def is_leap_year(year):
+    
+    # 4'e tam bölünen yıllar artık yıldır
+    #100'e tam bölünenler artık yıl değildir
+    #400'e tam bölünen yıllar tekrar artık yıldır.
 
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
     return False
 
-# Kullanıcıdan yıl al
+# Kullanıcıdan yil al
 try:
     year = int(input("Lutfen bir yil girin: "))
     if is_leap_year(year):
-        print(f"{year} bir artık yildır.")
+        print(f"{year} bir artik yildir.")
     else:
-        print(f"{year} bir artık yil değildir.")
+        print(f"{year} bir artik yil değildir.")
 except ValueError:
     print("Lütfen geçerli bir yil girin!")
